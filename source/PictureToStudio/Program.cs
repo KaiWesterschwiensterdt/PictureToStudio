1d9ed30813d4e618b90aa4986d3c2953687ce14d
Kai Westerschwiensterdt <K.Westerschwiensterdt@beckhoff.com>
2023-01-10 09:03:50 +0100

    Add project

---
+++ source/PictureToStudio/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+var converter = new PictureConverter();
+Console.WriteLine("Hello and welcome to our personal image enhancer!\r");
+Console.WriteLine("First enter the path to the image:");
+string inputPath = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Thank you. Now enter the path to the output directory:");
+string outputPath = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Last step. What should we call your new image?");
+string newFileName = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Sit back and wait for your new picture in the studio!");
+
+
+Image originalImage = Image.FromFile(inputPath);
+Image enhancedImage = converter.ToStudio((Bitmap)originalImage);
+enhancedImage.Save($"{outputPath}\\{newFileName}.png", ImageFormat.Png);
+
+Console.WriteLine($"Your {newFileName} is no available at {outputPath}!");
+Console.ReadLine();
